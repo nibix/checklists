8a5614819e3bb76d428bcabb9c4a2fb71edfada8
Nils Bandener <git@bndnr.de>
2024-06-25 01:39:29 +0200

    New tests for CheckList

--- src/test/java/com/selectivem/check/CheckListTest.java
+++ src/test/java/com/selectivem/check/CheckListTest.java
@@ -1,21 +1,5 @@
-/* 
+/*
  * Copyright 2024 Nils Bandener
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * Based on code which is:
- * 
- * Copyright 2022-2024 floragunn GmbH
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -34,167 +18,204 @@
 
 package com.selectivem.check;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class CheckListTest {
+    final Set<String> elements;
+    final CheckList<String> subject;
+    final int count;
+    final String oneElement;
+    final Set<String> someElements;
+
     @Test
     public void checkIf() {
-        CheckList<String> subject = CheckList.create(setOf("a", "b", "xxx", "c", "d"));
-        subject.checkIf((e) -> e.length() > 1);
-        Assert.assertEquals(setOf("xxx"), subject.getCheckedElements());
-        subject.checkIf((e) -> e.length() <= 1);
-        Assert.assertEquals(setOf("a", "b", "xxx", "c", "d"), subject.getCheckedElements());
+        subject.checkIf((e) -> e.equals(oneElement));
+        Assert.assertEquals(setOf(oneElement), subject.getCheckedElements());
+        subject.checkIf((e) -> !e.equals(oneElement));
+        Assert.assertEquals(elements, subject.getCheckedElements());
     }
 
     @Test
     public void uncheckIf() {
-        CheckList<String> subject = CheckList.create(setOf("a", "b", "xxx", "c", "d"));
         subject.checkAll();
-        subject.uncheckIf((e) -> e.length() > 1);
-
-        Assert.assertEquals(setOf("a", "b", "c", "d"), subject.getCheckedElements());
+        subject.uncheckIf((e) -> e.equals(oneElement));
+        Assert.assertEquals(without(elements, oneElement), subject.getCheckedElements());
     }
 
     @Test
-    public void checkIf_2e() {
-        CheckList<String> subject = CheckList.create(setOf("a", "xxx"));
-        subject.checkIf((e) -> e.length() > 1);
-        Assert.assertEquals(setOf("xxx"), subject.getCheckedElements());
-        subject.checkIf((e) -> e.length() <= 1);
-        Assert.assertEquals(setOf("a", "xxx"), subject.getCheckedElements());
+    public void uncheckIf_blank() {
+        subject.uncheckIf((e) -> {
+            Assert.fail("Should not be called for " + e);
+            return true;
+        });
     }
 
-    @Test
-    public void uncheckIf_2e() {
-        CheckList<String> subject = CheckList.create(setOf("a", "xxx"));
-        subject.checkAll();
-        subject.uncheckIf((e) -> e.length() > 1);
-
-        Assert.assertEquals(setOf("a"), subject.getCheckedElements());
-    }
-
-    @Test
+    @Test(expected = IllegalArgumentException.class)
     public void check_unknown() {
-        CheckList<String> subject = CheckList.create(setOf("a", "b", "c", "d"));
-
-        try {
-            subject.check("xxx");
-            Assert.fail();
-        } catch (IllegalArgumentException e) {
-
-        }
+        subject.check("xxx");
     }
 
-    @Test
-    public void check_unknown_2e() {
-        CheckList<String> subject = CheckList.create(setOf("a", "b"));
 
-        try {
-            subject.check("xxx");
-            Assert.fail();
-        } catch (IllegalArgumentException e) {
-
-        }
-    }
-
-    @Test
-    public void uncheck_unknown_2e() {
-        CheckList<String> subject = CheckList.create(setOf("a", "b"));
-
-        try {
-            subject.uncheck("xxx");
-            Assert.fail();
-        } catch (IllegalArgumentException e) {
-
-        }
+    @Test(expected = IllegalArgumentException.class)
+    public void uncheck_unknown() {
+        subject.uncheck("xxx");
     }
 
     @Test
     public void uncheckIfPresent() {
-        CheckList<String> subject = CheckList.create(setOf("a", "b", "c", "d"));
-        subject.check("a");
-        subject.check("b");
-        subject.uncheckIfPresent("a");
-        Assert.assertEquals(setOf("b"), subject.getCheckedElements());
-        Assert.assertEquals(setOf("a", "c", "d"), subject.getUncheckedElements());
+        subject.checkAll();
+        subject.uncheckIfPresent(oneElement);
+        Assert.assertEquals(without(elements, oneElement), subject.getCheckedElements());
+        Assert.assertEquals(setOf(oneElement), subject.getUncheckedElements());
     }
 
     @Test
-    public void uncheckIfPresent_2e() {
-        CheckList<String> subject = CheckList.create(setOf("a", "b"));
-        subject.check("a");
-        subject.check("b");
-        subject.uncheckIfPresent("a");
-        Assert.assertEquals(setOf("b"), subject.getCheckedElements());
-        Assert.assertEquals(setOf("a"), subject.getUncheckedElements());
+    public void uncheckIfPresent_blank() {
+        for (String element : elements) {
+            if (!element.equals(oneElement)) {
+                subject.check(element);
+            }
+        }
+        Assert.assertFalse(subject.isChecked(oneElement));
+        subject.uncheckIfPresent(oneElement);
+        Assert.assertFalse(subject.isChecked(oneElement));
+        Assert.assertEquals(without(elements, oneElement), subject.getCheckedElements());
     }
 
     @Test
     public void uncheckIfPresent_unknown() {
-        CheckList<String> subject = CheckList.create(setOf("a", "b", "c", "d"));
-        subject.check("a");
+        subject.checkAll();
         subject.uncheckIfPresent("xxx");
-        Assert.assertEquals(setOf("a"), subject.getCheckedElements());
-        Assert.assertEquals(setOf("b", "c", "d"), subject.getUncheckedElements());
+        Assert.assertEquals(elements, subject.getCheckedElements());
+        Assert.assertEquals(setOf(), subject.getUncheckedElements());
     }
 
     @Test
-    public void uncheckIfPresent_unknown_2e() {
-        CheckList<String> subject = CheckList.create(setOf("a", "b"));
-        subject.check("a");
-        subject.uncheckIfPresent("xxx");
-        Assert.assertEquals(setOf("a"), subject.getCheckedElements());
-        Assert.assertEquals(setOf("b"), subject.getUncheckedElements());
-    }
-
-    @Test
-    public void isBlank_2e() {
-        CheckList<String> subject = CheckList.create(setOf("a", "b"));
+    public void isBlank() {
         Assert.assertTrue(subject.isBlank());
-        subject.check("a");
+        subject.check(oneElement);
         Assert.assertFalse(subject.isBlank());
     }
 
     @Test
-    public void isComplete_2e() {
-        CheckList<String> subject = CheckList.create(setOf("a", "b"));
+    public void isComplete() {
         Assert.assertFalse(subject.isComplete());
-        Assert.assertFalse(subject.check("a"));
-        Assert.assertTrue(subject.check("b"));
+
+        for (String e : elements) {
+            subject.check(e);
+        }
+
         Assert.assertTrue(subject.isComplete());
     }
 
     @Test
-    public void isChecked_2e() {
-        CheckList<String> subject = CheckList.create(setOf("a", "b"));
-        Assert.assertFalse(subject.isChecked("a"));
-        Assert.assertFalse(subject.isChecked("b"));
-        subject.check("a");
-        Assert.assertTrue(subject.isChecked("a"));
-        Assert.assertFalse(subject.isChecked("b"));
-        subject.check("b");
-        Assert.assertTrue(subject.isChecked("a"));
-        Assert.assertTrue(subject.isChecked("b"));
+    public void isChecked() {
+        for (String e : elements) {
+            Assert.assertFalse(subject.isChecked(e));
+        }
+
+        subject.check(oneElement);
+
+        for (String e : elements) {
+            if (e.equals(oneElement)) {
+                Assert.assertTrue(subject.isChecked(e));
+            } else {
+                Assert.assertFalse(subject.isChecked(e));
+            }
+        }
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void isChecked_unknown() {
+        subject.uncheck("xxx");
     }
 
     @Test
-    public void isChecked_unknown_2e() {
-        CheckList<String> subject = CheckList.create(setOf("a", "b"));
-        try {
-            subject.uncheck("xxx");
-            Assert.fail();
-        } catch (IllegalArgumentException e) {
+    public void getCheckedElements_unknown() {
+        subject.checkAll();
+        Assert.assertFalse(subject.getCheckedElements().contains("xxx"));
+    }
 
+    @Test(expected = NoSuchElementException.class)
+    public void getCheckedElements_overrun() {
+        subject.checkAll();
+
+        Iterator<String> iter = subject.getCheckedElements().iterator();
+
+        while (iter.hasNext()) {
+            iter.next();
         }
+
+        iter.next();
+    }
+
+    @Test
+    public void getUncheckedElements() {
+        Assert.assertEquals(elements, subject.getUncheckedElements());
+        subject.checkAll();
+        Assert.assertEquals(setOf(), subject.getUncheckedElements());
+    }
+
+    @Test
+    public void getUncheckedElements_unknown() {
+        Assert.assertFalse(subject.getCheckedElements().contains("xxx"));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void getUncheckedElements_overrun() {
+        subject.checkAll();
+
+        Iterator<String> iter = subject.getUncheckedElements().iterator();
+
+        while (iter.hasNext()) {
+            iter.next();
+        }
+
+        iter.next();
+    }
+
+    @Test
+    public void getElements() {
+        Assert.assertEquals(elements, subject.getElements());
+    }
+
+
+    @Parameterized.Parameters(name = "{0}")
+    public static Collection<Object[]> params() {
+        return Arrays.asList(new Object[]{setOf("a")}, new Object[]{setOf("a", "b")}, new Object[]{setOf("a", "b", "c", "d")}, new Object [] {IntStream.rangeClosed(1, 1000).mapToObj(Integer::toString).collect(Collectors.toSet())});
+    }
+
+    public CheckListTest(Set<String> elements) {
+        this.elements = elements;
+        this.subject = CheckList.create(elements);
+        this.count = elements.size();
+        this.oneElement = elements.iterator().next();
+        this.someElements = elements.size() == 1 ? elements : minusOne(elements);
     }
 
     @SafeVarargs
     static <E> Set<E> setOf(E... elements) {
         return new HashSet<>(Arrays.asList(elements));
     }
+
+    static <E> Set<E> minusOne(Set<E> set) {
+        HashSet<E> result = new HashSet<>(set);
+        result.remove(set.iterator().next());
+        return result;
+    }
+
+    static <E> Set<E> without(Set<E> set, E element) {
+        HashSet<E> result = new HashSet<>(set);
+        result.remove(element);
+        return result;
+    }
+
 }
